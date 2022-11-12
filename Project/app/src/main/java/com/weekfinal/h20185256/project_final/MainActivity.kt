9a7f1978a065ca5fd4e8ae6785de12e2b1cc9a7b
AnSuHan <glorygem195@gmail.com>
2022-11-12 21:07:21 +0900

    Add init Project File

---
+++ Project/app/src/main/java/com/weekfinal/h20185256/project_final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.weekfinal.h20185256.project_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
